c3c76690cf59b17a77666cbc2d8d4cb1a44a0884
Lucas González Pascual <lucasgonzalezpascual@gmail.com>
2014-08-13 16:27:45 +0200

    Bug solved in Kml export

--- documents/initial_data.js
+++ documents/initial_data.js
@@ -1,43 +1,76 @@
+/*
+ * 1.- Open mongo console
+ * 2.- Execute command: load("<path>/initial_data.js")
+ */
+
+dbName = "competition-db2";
+conn = new Mongo();
+db = conn.getDB(dbName);
+
 /* Competition states */
-"competition-db".competitionState.insert({
+db.competitionState.insert({
     "name": "Open",
     "description": "Open to new participants"
 });
-"competition-db".competitionState.insert({
+db.competitionState.insert({
     "name": "Closed",
     "description": "Closed to new participants"
 });
-"competition-db".competitionState.insert({
+db.competitionState.insert({
     "name": "Ongoing",
     "description": "Currently taking place!"
 });
-"competition-db".competitionState.insert({
+db.competitionState.insert({
     "name": "Finished",
     "description": "Finished"
 });
 
 /* Competition types */
-"competition-db".competitionType.insert({
+db.competitionType.insert({
     "name": "5K",
     "description": "5 kilometer race"
 });
-"competition-db".competitionType.insert({
+db.competitionType.insert({
     "name": "10K",
     "description": "10 kilometer race"
 });
-"competition-db".competitionType.insert({
+db.competitionType.insert({
     "name": "Marathon",
     "description": "42 kilometer race"
 });
 
 /* Role types */
-"competition-db".roleType.insert({
+db.roleType.insert({
     "name": "MODERATOR",
     "description": "Users with this role are allowed to remove user's comments or participants of a competitions."
 });
 
-"competition-db".roleType.insert({
+db.roleType.insert({
     "name": "ADMIN",
     "description": "Users with this role are allowed to do almost everything within the application."
 });
 
+var adminRoleId = db.roleType.find({
+    name: {$eq: "ADMIN"}
+})[0]._id.valueOf();
+
+/* Default admin user */
+db.user.insert({
+    "username": "admin",
+    "password": "admin",
+    "name": "admin",
+    "surname": "admin",
+    "email": "admin@admin.com"
+});
+
+var adminUserId = db.user.find({
+    username: {$eq: "admin"}
+})[0]._id.valueOf();
+
+/* Assign ADMIN role to admin user */
+db.userRole.insert({
+    "userId": adminUserId,
+    "roleTypesId": [
+        adminRoleId
+    ]
+});
--- service-impl/src/main/java/es/lucasgp/cait/tfg/competition/service/impl/TrackingServiceImpl.java
+++ service-impl/src/main/java/es/lucasgp/cait/tfg/competition/service/impl/TrackingServiceImpl.java
@@ -1,6 +1,9 @@
 package es.lucasgp.cait.tfg.competition.service.impl;
 
+import de.micromata.opengis.kml.v_2_2_0.Document;
 import de.micromata.opengis.kml.v_2_2_0.Kml;
+import de.micromata.opengis.kml.v_2_2_0.LineString;
+import de.micromata.opengis.kml.v_2_2_0.MultiGeometry;
 import de.micromata.opengis.kml.v_2_2_0.Placemark;
 import es.lucasgp.cait.tfg.competition.dao.api.TrackingDao;
 import es.lucasgp.cait.tfg.competition.model.Tracking;
@@ -32,13 +35,21 @@ public class TrackingServiceImpl extends BaseServiceImpl<Tracking, String> imple
 
     @Override
     public Kml convertToKML(GeoJson geoJson) {
+
         final Kml kml = new Kml();
-        geoJson.getFeatures().forEach(feature -> {
-            Placemark placemark = kml.createAndSetPlacemark();
-            placemark.createAndSetTimeStamp().withWhen(
+        Document document = kml.createAndSetDocument();
+        Placemark linePlacemark = document.createAndAddPlacemark();
+        LineString lineString = linePlacemark.createAndSetLineString();
+
+        for (Feature feature : geoJson.getFeatures()) {
+
+            lineString.addToCoordinates(feature.getGeometry().getCoordinates()[0], feature.getGeometry().getCoordinates()[1]);
+
+            Placemark pointPlacemark = document.createAndAddPlacemark();
+            pointPlacemark.createAndSetTimeStamp().withWhen(
                 DateTimeFormatter.ISO_DATE_TIME.format(LocalDateTime.ofInstant(Instant.ofEpochMilli(feature.getProperties().getTimestamp().getTime()), ZoneId.systemDefault())));
-            placemark.createAndSetPoint().addToCoordinates(feature.getGeometry().getCoordinates()[0], feature.getGeometry().getCoordinates()[1]);
-        });
+            pointPlacemark.createAndSetPoint().addToCoordinates(feature.getGeometry().getCoordinates()[0], feature.getGeometry().getCoordinates()[1]);
+        }
         return kml;
     }
 }
