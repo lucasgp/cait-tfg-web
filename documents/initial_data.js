92098aa85cec9e89850beeb45a3d27180284b750
Lucas González Pascual <lucasgonzalezpascual@gmail.com>
2014-08-29 16:20:02 +0200

    Versionado de las entidades del script de inicialización de la base de datos.

--- documents/initial_data.js
+++ documents/initial_data.js
@@ -3,51 +3,60 @@
  * 2.- Execute command: load("<path>/initial_data.js")
  */
 
-dbName = "competition-db2";
+dbName = "competition-db";
 conn = new Mongo();
 db = conn.getDB(dbName);
 
 /* Competition states */
 db.competitionState.insert({
     "name": "Open",
-    "description": "Open to new participants"
+    "description": "Open to new participants",
+    "version": 0
 });
 db.competitionState.insert({
     "name": "Closed",
-    "description": "Closed to new participants"
+    "description": "Closed to new participants",
+    "version": 0
 });
 db.competitionState.insert({
     "name": "Ongoing",
-    "description": "Currently taking place!"
+    "description": "Currently taking place!",
+    "version": 0
 });
 db.competitionState.insert({
     "name": "Finished",
-    "description": "Finished"
+    "description": "Finished",
+    "version": 0
 });
 
 /* Competition types */
 db.competitionType.insert({
     "name": "5K",
-    "description": "5 kilometer race"
+    "description": "5 kilometer race",
+    "version": 0
 });
 db.competitionType.insert({
     "name": "10K",
-    "description": "10 kilometer race"
+    "description": "10 kilometer race",
+    "version": 0
 });
 db.competitionType.insert({
     "name": "Marathon",
-    "description": "42 kilometer race"
+    "description": "42 kilometer race",
+    "version": 0
 });
 
 /* Role types */
 db.roleType.insert({
     "name": "MODERATOR",
-    "description": "Users with this role are allowed to remove user's comments or participants of a competitions."
+    "description": "Users with this role are allowed to remove user's comments or participants of a competitions.",
+    "version": 0
 });
 
 db.roleType.insert({
     "name": "ADMIN",
-    "description": "Users with this role are allowed to do almost everything within the application."
+    "description": "Users with this role are allowed to do almost everything within the application.",
+    "version": 0
 });
 
 var adminRoleId = db.roleType.find({
@@ -60,7 +69,8 @@ db.user.insert({
     "password": "admin",
     "name": "admin",
     "surname": "admin",
-    "email": "admin@admin.com"
+    "email": "admin@admin.com",
+    "version": 0
 });
 
 var adminUserId = db.user.find({
@@ -72,5 +82,6 @@ db.userRole.insert({
     "userId": adminUserId,
     "roleTypesId": [
         adminRoleId
-    ]
+    ],
+    "version": 0
 });
